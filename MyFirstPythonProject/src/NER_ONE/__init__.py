d5f93d672a3ee6a085dcfffd1e5f681732d4138e
Bob <rrakshith007@gmail.com>
2019-11-29 11:04:23 +0530

    pythonprojectCommit

---
+++ MyFirstPythonProject/src/NER_ONE/__init__.py
@@ -0,0 +1,58 @@
+import spacy 
+
+nlp = spacy.load('en_core_web_sm') 
+  
+sentence = "Apple is looking at buying U.K. startup for $1 billion"
+  
+doc = nlp(sentence) 
+  
+for ent in doc.ents: 
+    print(ent.text, ent.label_) 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
